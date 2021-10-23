2c162e393f3a7e36322057c0e81513a197db5c8f
Aliaksandr Mikhin <aliaksandr_mikhin@epam.com>
2021-10-23 20:25:09 +0300

    feat (multithreading): implement task 4 with ThreadPool
    Implement task 4 with ThreadPool.

--- 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
+++ 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
@@ -18,6 +18,7 @@ namespace MultiThreading.Task4.Threads.Join
     {
         static readonly int maxThreadsCount = 10;
         static int threadsCount = 1;
+        static readonly Semaphore semaphore = new Semaphore(maxThreadsCount, maxThreadsCount);
 
         static void Main(string[] args)
         {
@@ -30,12 +31,18 @@ namespace MultiThreading.Task4.Threads.Join
 
             Console.WriteLine();
 
-            RunTaskWithThread(50);
+            const int start = 50;
+
+            Console.WriteLine("Task using threads");
+            RunTaskWithThread(start);
+
+            Console.WriteLine("Task using thread pool");
+            RunTaskWithThreadPool(start);
 
             Console.ReadLine();
         }
 
-        static void RunTaskWithThread(int start)
+        static void RunTaskWithThread(object parameters)
         {
             if (threadsCount > maxThreadsCount)
             {
@@ -43,11 +50,28 @@ namespace MultiThreading.Task4.Threads.Join
             }
 
             Interlocked.Add(ref threadsCount, 1);
-            var thread = new Thread((i) => RunTaskWithThread((int)i));
-            start--;
-            Console.WriteLine("Number " + start);
-            thread.Start(start);
+            var thread = new Thread(new ParameterizedThreadStart(RunTaskWithThread));
+            int number = (int)parameters;
+            ProcessNumber(ref number);
+            thread.Start(number);
             thread.Join();
+        }
+
+        static void RunTaskWithThreadPool(object parameters)
+        {
+            if (semaphore.WaitOne())
+            {
+                int number = (int)parameters;
+                ProcessNumber(ref number);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(RunTaskWithThreadPool), number);
+            }
+            
+        }
+
+        static void ProcessNumber(ref int number)
+        {
+            number--;
+            Console.WriteLine("Number " + number);
         }
     }
 }
