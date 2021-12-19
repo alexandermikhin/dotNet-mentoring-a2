8e101d71401034e25651dcf2e17a8037dcb8b2c7
Aliaksandr Mikhin <aliaksandr_mikhin@epam.com>
2021-12-20 00:43:33 +0300

    feat (patterns): add classes for inputText and label elements
    Add classes for inputText and label elements.

---
+++ 07.patterns/Patterns/Composite/Elements.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml;
+
+namespace Composite
+{
+    public abstract class Element
+    {
+        protected XmlElement xmlElement;
+        protected readonly string elementName;
+        private readonly XmlDocument xmlDocument = new XmlDocument();
+
+        protected Element(string name)
+        {
+            xmlElement = xmlDocument.CreateElement(name);
+        }
+
+        public abstract string ConvertToString();
+    }
+
+    public class InputText: Element
+    {
+        public InputText(string name, string value): base("inputText")
+        {
+            xmlElement.SetAttribute("name", name);
+            xmlElement.SetAttribute("value", value);
+        }
+
+        public override string ConvertToString()
+        {
+            return xmlElement.OuterXml;
+        }
+    }
+    public class LabelText: Element
+    {
+        public LabelText(string value): base("label")
+        {
+            xmlElement.SetAttribute("value", value);
+        }
+
+        public override string ConvertToString()
+        {
+            return xmlElement.OuterXml;
+        }
+    }
+
+}
---
+++ 07.patterns/Patterns/Composite/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = new Element[]
+            {
+                new InputText("myInput", "myInputValue"),
+                new LabelText("myLabel"),
+            };
+
+            foreach (var element in elements)
+            {
+                Console.WriteLine(element.ConvertToString());
+            }
+        }
+    }
+}
