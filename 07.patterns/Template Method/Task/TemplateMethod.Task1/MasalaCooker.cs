f491e43919300aa49a4590e7ddb171e8ea8a651b
Aliaksandr Mikhin <aliaksandr_mikhin@epam.com>
2021-12-22 23:53:55 +0300

    feat (patterns): implement task 1 for Template Method pattern
    Implement task 1 for Template Method pattern.

--- 07.patterns/Template Method/Task/TemplateMethod.Task1/MasalaCooker.cs
+++ 07.patterns/Template Method/Task/TemplateMethod.Task1/MasalaCooker.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace TemplateMethod.Task1
+﻿namespace TemplateMethod.Task1
 {
     public class MasalaCooker
     {
@@ -13,7 +11,56 @@ namespace TemplateMethod.Task1
 
         public void CookMasala(Country country)
         {
-            throw new NotImplementedException();
+            CookRice(country);
+            CookChicken(country);
+            CookTea(country);
+        }
+
+        private void CookRice(Country country)
+        {
+            switch (country)
+            {
+                case Country.India:
+                    cooker.FryRice(200, Level.Strong);
+                    cooker.SaltRice(Level.Strong);
+                    cooker.PepperRice(Level.Strong);
+                    break;
+                case Country.Ukraine:
+                    cooker.FryRice(500, Level.Strong);
+                    cooker.SaltRice(Level.Strong);
+                    cooker.PepperRice(Level.Low);
+                    break;
+            }
+        }
+
+        private void CookChicken(Country country)
+        {
+            switch (country)
+            {
+                case Country.India:
+                    cooker.FryChicken(100, Level.Strong);
+                    cooker.SaltChicken(Level.Strong);
+                    cooker.PepperChicken(Level.Strong);
+                    break;
+                case Country.Ukraine:
+                    cooker.FryChicken(300, Level.Medium);
+                    cooker.SaltChicken(Level.Medium);
+                    cooker.PepperChicken(Level.Low);
+                    break;
+            }
+        }
+
+        private void CookTea(Country country)
+        {
+            switch (country)
+            {
+                case Country.India:
+                    cooker.PrepareTea(15, TeaKind.Green, 12);
+                    break;
+                case Country.Ukraine:
+                    cooker.PrepareTea(10, TeaKind.Black, 10);
+                    break;
+            }
         }
     }
 }
