a106cad3f7ced34f1f5c2306ba34ee655f98e041
Alexander Mikhin <alexmikh@tut.by>
2021-12-26 01:01:42 +0300

    feat (patterns): add initial implementation of Feed Manager task 1
    Add initial implementation of Feed Manager task 1.

---
+++ 07.patterns/Factory Method/Feed Manager/FeedManager.Task1/FeedValidators/BaseFeedValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FeedManager.Task1.FeedImporters;
+using FeedManager.Task1.Feeds;
+
+namespace FeedManager.Task1.FeedValidators
+{
+    public abstract class BaseFeedValidator
+    {
+        protected ValidateResult validateResult;
+
+        protected void ValidateIds(TradeFeed feed)
+        {
+            if (feed.StagingId < 1 || feed.CounterpartyId < 1 || feed.PrincipalId < 1 || feed.SourceAccountId < 1)
+            {
+                validateResult.Errors.Add(ErrorCode.IdIsNotValidMessage);
+            }
+        }
+
+        protected void ValidatePrice(TradeFeed feed)
+        {
+            if (feed.CurrentPrice < 0 || !HasValidFractionPart(feed.CurrentPrice, 2))
+            {
+                validateResult.Errors.Add(ErrorCode.PriceIsNotValid);
+            }
+        }
+
+        protected void UpdateValidState()
+        {
+            validateResult.IsValid = validateResult.Errors.Count == 0;
+        }
+
+        private bool HasValidFractionPart(decimal number, int decimals)
+        {
+            return number - Math.Round(number, decimals) == 0;
+        }
+    }
+}
